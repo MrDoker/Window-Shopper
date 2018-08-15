0fe3d35f9df8c61d903cc3f7f346ef82e5930974
George Sazonov <g-doker@yandex.ru>
2018-08-16 01:13:07 +0300

    1.Create Calculate button 2.Integrate showing of custom UI class in StoryBoard 3. Beginning of unit testing

--- Window Shopper/Controller/MainViewController.swift
+++ Window Shopper/Controller/MainViewController.swift
@@ -17,11 +17,19 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let calculateButtom = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 80))
+        let calculateButtom = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
         calculateButtom.backgroundColor = #colorLiteral(red: 0.9692655457, green: 0.5622963727, blue: 0.01247087664, alpha: 1)
+        calculateButtom.setTitle("Calculate", for: .normal)
+        calculateButtom.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calculateButtom.addTarget(self, action: #selector(calculate), for: .touchUpInside)
         
+        wageTextField.inputAccessoryView = calculateButtom
+        priceTextField.inputAccessoryView = calculateButtom
     }
 
-
+    @objc func calculate() {
+        
+    }
+    
 }
 
